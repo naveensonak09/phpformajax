a434b0f7fa35dc9b0190277ea81e1bc469ff538d
naveen kumar <naveen_kumar@excellencetechnologies.in>
2020-08-04 10:55:22 +0530

    php modal form ajax

---
+++ includes/config.php
@@ -0,0 +1,3 @@
+<?php
+     $config = mysqli_connect("localhost","root","123","user_info") or die("connection wrong !!");
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>table</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        <script src="js/"></script>
+    </head>
+    <body>
+        <div class="container">
+           <h3>submit form using ajax php</h3>
+         </div>
+    </body>
+</html>
+
